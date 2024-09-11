489f6a40816b0dc0511861125ea0c23dd2556e24
zoe4everyang <344769005@qq.com>
2024-09-11 13:58:52 -0400

    environment setup

---
+++ jobAI/src/main/java/com/talaria/jobAI/JobAiApplication.java
@@ -0,0 +1,13 @@
+package com.talaria.jobAI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobAiApplication.class, args);
+	}
+
+}
---
+++ jobAI/src/test/java/com/talaria/jobAI/JobAiApplicationTests.java
@@ -0,0 +1,13 @@
+//package com.talaria.jobAI;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class JobAiApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
